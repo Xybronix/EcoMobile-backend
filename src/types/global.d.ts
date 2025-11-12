3a75af5c69f845297306cf78d8aac738af2eaeb7
Brayan-Weko <wekobrayan163@gmail.com>
2025-11-12 18:25:36 +0100

    V1

---
+++ src/types/global.d.ts
@@ -0,0 +1,8 @@
+// types/global.d.ts
+declare module 'pg';
+declare module 'swagger-jsdoc';
+declare module 'jsonwebtoken';
+declare module 'morgan';
+declare module 'swagger-ui-express';
+declare module 'bcryptjs';
+declare module 'nodemailer';
